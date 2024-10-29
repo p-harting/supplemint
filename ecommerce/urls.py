0c0c21d94f79c02ec07be9b44c03a8a6607ee63b
Pascal Harting <pascalharting98@gmail.com>
2024-10-29 12:33:34 +0000

    Add products views, urls and template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ]
 
 if settings.DEBUG:
