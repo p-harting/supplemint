d1301970265478c92535008983a993fc25b3aec0
Pascal Harting <pascalharting98@gmail.com>
2024-11-02 11:39:14 +0000

    Add bag context and delivery functionality

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('products/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ]
 
 if settings.DEBUG:
