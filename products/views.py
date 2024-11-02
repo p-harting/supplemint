d9b3358037e1280434c4a7d5a787895e6afcd532
Pascal Harting <pascalharting98@gmail.com>
2024-11-02 09:35:30 +0000

    Add sorting to products page

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category, SubCategory
+from django.db.models.functions import Lower
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
@@ -20,6 +21,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
