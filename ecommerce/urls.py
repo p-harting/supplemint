f280c5c736eccc91d11fdf8e6937bd9bda28f9b2
Pascal Harting <pascalharting98@gmail.com>
2024-11-04 13:20:49 +0000

    Add views and urls to checkout app

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ]
 
 if settings.DEBUG:
