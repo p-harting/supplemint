44f80a9fd29bf81a439d548a8daf5ef42fe046cd
Pascal Harting <pascalharting98@gmail.com>
2024-11-06 06:43:14 +0000

    Remove secret key and set debug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i#+@#_r91#9yr&gdm(iu1a$+ajy3p3436)min6!u_(xt^rv3e^'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', '8000-pharting-supplemint-evq9f9h4spd.ws.codeinstitute-ide.net', 'supplemint-91663a1226fd.herokuapp.com', ]
 
