621f319f7785c467706ed7d1d570883ac08f4017
Pascal Harting <pascalharting98@gmail.com>
2024-12-15 13:06:46 +0000

    Add styling to blog.css, fix bug and import in urls and models

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.text import slugify
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
