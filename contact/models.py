b614c09e8a560f10baec2c7445b563077d933a09
Pascal Harting <pascalharting98@gmail.com>
2024-12-15 12:29:34 +0000

    Create blog app, add to installed apps and create basic model

--- contact/models.py
+++ contact/models.py
@@ -1,4 +1,3 @@
-# contact/models.py
 from django.db import models
 
 class ContactMessage(models.Model):
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'profiles',
     'storages',
     'contact',
+    'blog',
 ]
 
 MIDDLEWARE = [
