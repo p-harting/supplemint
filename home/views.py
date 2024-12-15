f513d5ea9782c957e1e827dcc03729cd99f64447
Pascal Harting <pascalharting98@gmail.com>
2024-12-15 15:02:44 +0000

    Add categories, random recommended product and newsletter to index

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from products.models import Product
+import random
 
 def index(request):
-    return render(request, 'home/index.html')
+    all_products = Product.objects.all()
+
+    if len(all_products) >= 4:
+        random_products = random.sample(list(all_products), 6)
+    else:
+        random_products = all_products
+
+    return render(request, 'home/index.html', {'random_products': random_products})
 
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
