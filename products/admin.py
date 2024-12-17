3f63da5b2d9d89fdbc84f76a0d0ee7d618c2a281
Pascal Harting <pascalharting98@gmail.com>
2024-12-17 12:56:25 +0000

    Add new product information into model and admin

--- products/admin.py
+++ products/admin.py
@@ -10,8 +10,30 @@ class ProductAdmin(admin.ModelAdmin):
         'price',
         'rating',
         'image',
+        'detailed_description',
+        'nutritional_info',
+        'how_to_use',
+        'key_facts',
     )
     ordering = ('sku',)
+
+    fields = (
+        'sku',
+        'name',
+        'category',
+        'subcategory',
+        'description',
+        'detailed_description',
+        'nutritional_info',
+        'how_to_use',
+        'key_facts',
+        'has_sizes',
+        'price',
+        'rating',
+        'image',
+        'slug',
+    )
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
@@ -19,13 +41,14 @@ class CategoryAdmin(admin.ModelAdmin):
         'name',
     )
 
-# New SubCategoryAdmin
+
 class SubCategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
         'category',
     )
+
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from ckeditor.fields import RichTextField
 
 class Category(models.Model):
     class Meta:
@@ -14,6 +15,7 @@ class Category(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class SubCategory(models.Model):
     category = models.ForeignKey(Category, related_name='subcategories', on_delete=models.CASCADE)
     name = models.CharField(max_length=254)
@@ -25,21 +27,26 @@ class SubCategory(models.Model):
     def get_friendly_name(self):
         return self.friendly_name
 
+
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     subcategory = models.ForeignKey('SubCategory', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    detailed_description = RichTextField(null=True, blank=True)
     has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    nutritional_info = RichTextField(null=True, blank=True)
+    how_to_use = models.TextField(null=True, blank=True)
+    key_facts = models.JSONField(null=True, blank=True) 
     slug = models.SlugField(max_length=254, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
-    
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.name)
