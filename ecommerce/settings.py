f5b0097a353294e3e929cc034f42856d3e01d88c
Pascal Harting <pascalharting98@gmail.com>
2025-01-01 16:44:28 +0000

    Revert settings.py to remove exposed secrets

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -231,12 +231,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-#DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_USER = 'pascalharting98@gmail.com'
-EMAIL_HOST_PASSWORD = 'dekjztqfvgpzeuso'
-DEFAULT_FROM_EMAIL = 'pascalharting98@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # CKEditor Settings
 CKEDITOR_UPLOAD_PATH = 'uploads/'
