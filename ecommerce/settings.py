4a56bac09894753b0519f57e93b70802251a3dbb
Pascal Harting <pascalharting98@gmail.com>
2025-01-03 11:08:18 +0000

    Removed secret key

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', ')untd_i@_&f=zd+ux0&!w!^3d2dk3lz#0+6vd+!1=)k*!dt2m_')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 
 if os.environ.get('ENV') == 'development':
